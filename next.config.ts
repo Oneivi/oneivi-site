7019b7352ba41812d954e8ef4c42b352949ef9e9
Oneivi <90078023+Oneivi@users.noreply.github.com>
2025-10-29 14:48:48 +0000

    arreglos apra el deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 // next.config.ts
 import type { NextConfig } from "next";
-import { withContentlayer } from "next-contentlayer";
+import { withContentlayer } from "next-contentlayer2";
 
 const config: NextConfig = {
+  eslint: { ignoreDuringBuilds: true }, // evita el error "ESLint must be installed..."
   experimental: { forceSwcTransforms: true },
+  // Si usabas experimental.turbo, migra a:
+  // turbopack: { enabled: true },
 };
 
 export default withContentlayer(config);
