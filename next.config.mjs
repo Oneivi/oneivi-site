6e8343b4624cad80f772b1c574219835c0af0c2c
Oneivi <90078023+Oneivi@users.noreply.github.com>
2025-10-29 15:10:41 +0000

    chore: remove next.config.mjs and use next-contentlayer2 in TS config

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-// next.config.mjs
-import { withContentlayer } from 'next-contentlayer'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { turbo: { /* si usas turbopack */ } },
-}
-
-export default withContentlayer(nextConfig)
